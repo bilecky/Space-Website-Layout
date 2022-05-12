c7d1af53b5e5a7ac70979b52c6d7b88f7d62d447
Pawel <bilecky95@gmail.com>
2022-05-12 20:55:49 +0200

    added hamburger color change when section is white

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
 const navigation = document.querySelector('.navigation')
 const hamburgerBtn = document.querySelector('.container')
 const navItems = document.querySelectorAll('.navigation__item')
+const accordion = document.querySelectorAll('.accordion__box')
+const sectionsGSAP = document.querySelectorAll('.section-gsap')
+const svg = document.querySelector('svg')
+const allSections = document.querySelectorAll('.section')
+const header = document.querySelector('header')
+
 //nav handler
 const navHandler = () => {
 	navigation.classList.toggle('navigation--toggle-nav')
@@ -13,37 +19,73 @@ const navHandler = () => {
 }
 
 hamburgerBtn.addEventListener('click', navHandler)
-AOS.init();
+AOS.init()
+
+//hamburger color changer when section is white
+
+const hamburgerColorHandler = () => {
+	const currentSection = window.scrollY
+	console.log(currentSection)
+	allSections.forEach(section => {
+		switch (true) {
+			case section.classList.contains('section-white') &&
+				section.offsetTop <= currentSection + 130:
+				svg.style.stroke = '#7800b4'
+
+				break
+			case !section.classList.contains('section-white') &&
+				section.offsetTop <= currentSection:
+				svg.style.stroke = 'white'
+				console.log('gowno')
+
+				break
+		}
+	})
+}
+
+window.addEventListener('scroll', hamburgerColorHandler)
 
 //accordion
-const accordion = document.querySelectorAll('.accordion__box')
 
 accordion.forEach(item => {
 	item.addEventListener('click', () => {
 		item.classList.toggle('active')
-		console.log(item);
+		console.log(item)
 	})
 })
 // contact map leaflet
-var map = L.map('map').setView([21.24903, -157.95486], 7);
+const map = L.map('map').setView([21.24903, -157.95486], 7)
 
-L.tileLayer('https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=CMcrJPUIbvHEfLRgwdus',{
-	tileSize: 512,
-	zoomOffset: -1,
-	minZoom: 1,
-	attribution: "\u003ca href=\"https://www.maptiler.com/copyright/\" target=\"_blank\"\u003e\u0026copy; MapTiler\u003c/a\u003e \u003ca href=\"https://www.openstreetmap.org/copyright\" target=\"_blank\"\u003e\u0026copy; OpenStreetMap contributors\u003c/a\u003e",
-	crossOrigin: true
-  }).addTo(map);
+L.tileLayer(
+	'https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=CMcrJPUIbvHEfLRgwdus',
+	{
+		tileSize: 512,
+		zoomOffset: -1,
+		minZoom: 1,
+		attribution:
+			'\u003ca href="https://www.maptiler.com/copyright/" target="_blank"\u003e\u0026copy; MapTiler\u003c/a\u003e \u003ca href="https://www.openstreetmap.org/copyright" target="_blank"\u003e\u0026copy; OpenStreetMap contributors\u003c/a\u003e',
+		crossOrigin: true,
+	}
+).addTo(map)
 
-  //GSAP OBSERVER
+//GSAP OBSERVER
 
-gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(ScrollTrigger)
 
-const sections = document.querySelectorAll('.section-gsap');
-
-// sections.forEach(section => {
-//   gsap.fromTo(section.children, {y: '+=100', opacity: 0}, {y: 0, opacity: 1, stagger: 0.2, duration: 1, ease: 'easeInOut', scrollTrigger: {
-//   trigger: section,
-//   start: 'top 50%',
-// }});  
-// });
+sectionsGSAP.forEach(section => {
+	gsap.fromTo(
+		section.children,
+		{ y: '+=100', opacity: 0 },
+		{
+			y: 0,
+			opacity: 1,
+			stagger: 0.2,
+			duration: 1,
+			ease: 'easeInOut',
+			scrollTrigger: {
+				trigger: section,
+				start: 'top 50%',
+			},
+		}
+	)
+})
